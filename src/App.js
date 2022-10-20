d772382307590fa94c74ec3b7ed0e50472b6e2cb
Si Lam <silam@hotmail.com>
2022-10-19 23:41:24 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
+
+const URI = 'https://raw.githubusercontent.com/tabatkins/wordle-list/main/words'
+
+function Tile() {
+  return (
+    <div className="Tile">
+
+    </div>
+  )
+}
+
+function Board() {
+  return(
+    <div>
+        <Tile></Tile><Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
+    </div>
+    
+
+  )
+}
 function App() {
+
+  const [solution, setSolution] = useState('')
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const data = await fetch(URI);
+      const words = await (await data.text()).split('\n');
+      
+      console.log(words.length);
+      const randomWord = words[Math.floor(Math.random() * words.length)]
+      setSolution(randomWord);
+    }
+
+    fetchData();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{solution}</p>
+      <p>
+        <Board></Board>
+      </p>
     </div>
   );
 }
